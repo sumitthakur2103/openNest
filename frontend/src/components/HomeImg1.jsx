3aef408bb574ab9d33c9bea88f103166de14313d
Sumit Thakur <tsumit0505@gmail.com>
2025-08-20 00:18:53 +0530

    responsive design

--- frontend/src/components/HomeImg1.jsx
+++ frontend/src/components/HomeImg1.jsx
@@ -1,21 +1,33 @@
+// import React from "react";
+// import HotelImg from "../assets/Hotel1.jpg";
+
+// export default function HomeImg1() {
+//   return (
+//     <div
+//       style={{
+//         width: "100%",
+//         height: "100%",
+//         padding: "70px",
+//         backgroundColor: "black",
+//       }}
+//     >
+//       <img
+//         src={HotelImg}
+//         alt="Hotel"
+//         style={{ width: "100%", height: "100%", borderRadius: "20px" }}
+//       />
+//     </div>
+//   );
+// }
+
 import React from "react";
 import HotelImg from "../assets/Hotel1.jpg";
+import "../styles/HomeImg1.css"; // import CSS file
 
 export default function HomeImg1() {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        padding: "70px",
-        backgroundColor: "black",
-      }}
-    >
-      <img
-        src={HotelImg}
-        alt="Hotel"
-        style={{ width: "100%", height: "100%", borderRadius: "20px" }}
-      />
+    <div className="homeimg-container">
+      <img src={HotelImg} alt="Hotel" className="homeimg" />
     </div>
   );
 }
